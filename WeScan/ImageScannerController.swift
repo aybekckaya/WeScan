5cbe6c0e0779bcade7f36662dd584f2273b4b267
aybek can kaya <aybekcankaya@icloud.com>
2020-01-23 17:49:18 +0300

    Code modify

--- WeScan/ImageScannerController.swift
+++ WeScan/ImageScannerController.swift
@@ -63,13 +63,6 @@ public final class ImageScannerController: UINavigationController {
         super.init(rootViewController: self.scannerVC)
         
         self.imageScannerDelegate = delegate
-        
-        if #available(iOS 13.0, *) {
-            navigationBar.tintColor = .label
-        } else {
-            navigationBar.tintColor = .black
-        }
-        navigationBar.isTranslucent = false
         self.isNavigationBarHidden = true
         self.isToolbarHidden = true 
         self.view.addSubview(blackFlashView)
@@ -162,70 +155,4 @@ public final class ImageScannerController: UINavigationController {
     }
 }
 
-/// Data structure containing information about a scan, including both the image and an optional PDF.
-public struct ImageScannerScan {
-    public enum ImageScannerError: Error {
-        case failedToGeneratePDF
-    }
-    
-    public var image: UIImage
-    
-    public func generatePDFData(completion: @escaping (Result<Data, ImageScannerError>) -> Void) {
-        DispatchQueue.global(qos: .userInteractive).async {
-            if let pdfData = self.image.pdfData() {
-                completion(.success(pdfData))
-            } else {
-                completion(.failure(.failedToGeneratePDF))
-            }
-        }
-        
-    }
-    
-    mutating func rotate(by rotationAngle: Measurement<UnitAngle>) {
-        guard rotationAngle.value != 0, rotationAngle.value != 360 else { return }
-        image = image.rotated(by: rotationAngle) ?? image
-    }
-}
 
-/// Data structure containing information about a scanning session.
-/// Includes the original scan, cropped scan, detected rectangle, and whether the user selected the enhanced scan. May also include an enhanced scan if no errors were encountered.
-public struct ImageScannerResults {
-    
-    /// The original scan taken by the user, prior to the cropping applied by WeScan.
-    public var originalScan: ImageScannerScan
-    
-    /// The deskewed and cropped scan using the detected rectangle, without any filters.
-    public var croppedScan: ImageScannerScan
-    
-    /// The enhanced scan, passed through an Adaptive Thresholding function. This image will always be grayscale and may not always be available.
-    public var enhancedScan: ImageScannerScan?
-    
-    /// Whether the user selected the enhanced scan or not.
-    /// The `enhancedScan` may still be available even if it has not been selected by the user.
-    public var doesUserPreferEnhancedScan: Bool
-    
-    /// The detected rectangle which was used to generate the `scannedImage`.
-    public var detectedRectangle: Quadrilateral
-    
-    @available(*, unavailable, renamed: "originalScan")
-    public var originalImage: UIImage?
-    
-    @available(*, unavailable, renamed: "croppedScan")
-    public var scannedImage: UIImage?
-    
-    @available(*, unavailable, renamed: "enhancedScan")
-    public var enhancedImage: UIImage?
-    
-    @available(*, unavailable, renamed: "doesUserPreferEnhancedScan")
-    public var doesUserPreferEnhancedImage: Bool = false
-    
-    init(detectedRectangle: Quadrilateral, originalScan: ImageScannerScan, croppedScan: ImageScannerScan, enhancedScan: ImageScannerScan?, doesUserPreferEnhancedScan: Bool = false) {
-        self.detectedRectangle = detectedRectangle
-        
-        self.originalScan = originalScan
-        self.croppedScan = croppedScan
-        self.enhancedScan = enhancedScan
-        
-        self.doesUserPreferEnhancedScan = doesUserPreferEnhancedScan
-    }
-}
---
+++ WeScan/Models/ImageScannerScan.swift
@@ -0,0 +1,34 @@
+//
+//  ImageScannerScan.swift
+//  WeScan
+//
+//  Created by aybek can kaya on 23.01.2020.
+//  Copyright © 2020 WeTransfer. All rights reserved.
+//
+
+import Foundation
+
+/// Data structure containing information about a scan, including both the image and an optional PDF.
+public struct ImageScannerScan {
+    public enum ImageScannerError: Error {
+        case failedToGeneratePDF
+    }
+    
+    public var image: UIImage
+    
+    public func generatePDFData(completion: @escaping (Result<Data, ImageScannerError>) -> Void) {
+        DispatchQueue.global(qos: .userInteractive).async {
+            if let pdfData = self.image.pdfData() {
+                completion(.success(pdfData))
+            } else {
+                completion(.failure(.failedToGeneratePDF))
+            }
+        }
+        
+    }
+    
+    mutating func rotate(by rotationAngle: Measurement<UnitAngle>) {
+        guard rotationAngle.value != 0, rotationAngle.value != 360 else { return }
+        image = image.rotated(by: rotationAngle) ?? image
+    }
+}
